c7678a19fd2ac839e0e4532ab50d7c8f3b4d4cc6
oniharnantyo <oni.harnantyo97@gmail.com>
2019-11-17 22:19:16 +0700

    Feat - Favicon

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,7 +4,15 @@
     <title>@yield('title')</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <link rel="apple-touch-icon" sizes="180x180" href={{ url("/images/favicon/apple-touch-icon.png") }}>
+    <link rel="icon" type="image/png" sizes="32x32" href={{ url("/images/favicon/favicon-32x32.png") }}>
+    <link rel="icon" type="image/png" sizes="16x16" href={{ url("/images/favicon/favicon-16x16.png") }}>
+    <link rel="manifest" href={{ url("/images/favicon/site.webmanifest") }}>
+    <link rel="mask-icon" href={{ url("/images/favicon/safari-pinned-tab.svg") }} color="#5bbad5">
+    <link rel="shortcut icon" href={{ url("/images/favicon/favicon.ico") }}>
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content={{ url("/images/favicon/browserconfig.xml") }}>
+    <meta name="theme-color" content="#ffffff">
 
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700" rel="stylesheet">
 
