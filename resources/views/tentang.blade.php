598101b85e3f17ac6f8a7d5ec9ab25275fe29074
oniharnantyo <oni.harnantyo97@gmail.com>
2019-11-27 00:26:44 +0700

    Feat - About page

--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -6,11 +6,29 @@
             <div class="row align-items-center justify-content-center">
                 <div class="row mt-4">
                     <div class="col-12 text-center">
-                        <h2 class="section-title mb-3">Apa itu Vidyasena?</h2>
+                        <h2 class="section-title mb-5">Apa itu Vidyasena?</h2>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-12" data-aos="fade-up">
+                <div class="row justify-content-center">
+                    <div class="col-md-9" data-aos="fade-up">
+                        <p class="text-justify mx-4 mx-md-0 mx-lg-0">​<img class="img-fluid float-left mr-3" alt="A picture of wooden spoons with spices and herbs in them." src="https://pbs.twimg.com/profile_images/248185979/logo_Vidyasena.bmp" style="height:180px;" />Dalam bahasa Sansekerta, &ldquo;Vidya&rdquo; berarti pengetahuan Dhamma dan &ldquo;Sena&rdquo; berarti prajurit atau tentara penjaga, sehingga Vidyasena berarti &ldquo;prajurit pengetahuan Dhamma&rdquo;. Vidyasena mendapat pembinaan moral dan bimbingan Dhamma dari Sangha Theravada Indonesia dan majelis Pandita Buddha Dhamma Indonesia. Jadi sungguh tepat organisasi Vidyasena berkiprah di Vihara Vidyaloka (yang berarti &ldquo;tempat pengetahuan Dhamma&rdquo;).</p>
+
+                        <p class="text-justify mx-4 mx-md-0 mx-lg-0">&nbsp;</p>
+
+                        <h3 class="text-justify mx-4 mx-md-0 mx-lg-0"><strong>Sejarah</strong></h3>
+
+                        <p class="text-justify mx-4 mx-md-0 mx-lg-0">Pada akhir tahun 1986, Dharmanadi Chandra mengumpulkan beberapa mahasiswa maupun pelajar, antara lain Johny Chandra, Yenawati, Liliaswari, Ratna Sari, Erly, Luh Summedani, Cinho, Molly, Benny Wibowo, Murtini, Andikha, dan banyak lagi kawula muda Buddhis. Mereka kumpul untuk mengadakan kegiatan di Vihara Vidyaloka, Yogyakarta.</p>
+
+                        <p class="text-justify mmx-4 mx-md-0 mx-lg-0">Dengan dukungan dari Upasaka Pandita Soepomo, yang merupakan pembina Vihara Vidyaloka, dukungan moral dan bimbingan Buddha Dhamma dari beberapa anggota Sangha Theravada Indonesia pada waktu itu. Anggota sangha tersebut adalah yaitu Sekretaris Jenderal Sangha Theravada Indonesia Bhikkhu Pannyavaro, Samanera Jotidhammo dan Samanera Uttamo dan beberapa anggota sangha yang lain. Pada tanggal 1 Februari 1987 pukul 13.00 WIB, resmi dibentuk organisasi persaudaraan kawula muda Vidyasena.</p>
+
+                        <p class="text-justify mx-4 mx-md-0 mx-lg-0">&nbsp;</p>
+
+                        <h3 class="text-justify mx-4 mx-md-0 mx-lg-0"><strong>Visi dan Misi</strong></h3>
+
+                        <p class="text-justify mx-4 mx-md-0 mx-lg-0">
+                            <img class="img-fluid float-right ml-3" style="width:400px; " alt="" src="https://instagram.fsub1-2.fna.fbcdn.net/v/t51.2885-15/e35/32388929_196544717638386_7125674314015178752_n.jpg?_nc_ht=instagram.fsub1-2.fna.fbcdn.net&amp;_nc_cat=106&amp;oh=0edfda73488fcfb690815ac99574326d&amp;oe=5E6B2334" />
+                            Sebagai organisasi pemuda Buddhis yang anggotanya sebagian besar mahasiswa Buddhis dari berbagai kampus, Vidyasena mempunyai misi dan visi yang mencakup beberapa aspek. Pertama adalah aspek Buddha Dhamma, yaitu aspek yang meliputi kebenaran kesunyataan yang berlaku, bekerja, bersifat pasti, dan melingkupi seluruh kehidupan alam semesta ini. Dengan demikian diharapkan Vidyasena dapat memajukan perkembangan Buddha Dhamma dengan menggali dan memelihara ajaran agama Buddha berdasarkan kitab suci Tipitaka Pali, serta memberikan pembinaan kepada para mahasiswa, pelajar, dan generasi muda Buddhis dalam meningkatkan penghayatan dan pengamalan Buddha Dhamma di Yogyakarta pada khususnya dan di Indonesia pada umumnya. Kedua, aspek sumber daya manusia, yang dikembangkan dengan cara pelatihan dalam organisasi dan saling interaksi sebagai sarana untuk membina, meningkatkan, dan mengembangkan kepribadian, kemampuan, kecakapan anggota yang bertanggung jawab.
+                        </p>
 
                     </div>
                 </div>
